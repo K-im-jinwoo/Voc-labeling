05d6ed6fda6793367ec3ef20436732c7f2b5ed2d
gabang2 <offbeat1020@naver.com>
2023-01-10 01:32:00 +0900

    모델 수정

--- mainapp/models.py
+++ mainapp/models.py
@@ -28,7 +28,7 @@ class Review(models.Model):
     second_status = models.BooleanField(default=False)
     dummy_status = models.BooleanField(default=False)
     labeled_user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    first_assign = models.TextField(default="")
+    first_assign_user = models.TextField(default="0")
 
     def __str__(self):
         return str(self.review_id) + ' - ' + str(self.category_product)
