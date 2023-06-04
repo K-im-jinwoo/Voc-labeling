f75a76450059f7209a1051ab1222a6a84bb6d0eb
dongwoochoi <dong99woo@gmail.com>
2023-06-04 18:17:09 +0900

    Review 모델에 model_name,model_code항목 추가, csv파일 업로드시 업로드

--- mainapp/models.py
+++ mainapp/models.py
@@ -31,7 +31,8 @@ class Review(models.Model):
     labeled_user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     first_assign_user = models.TextField(default="0", null=True)
     second_assign_user = models.TextField(default="0", null=True)
-
+    model_name = models.TextField(default="", null=True)
+    model_code = models.TextField(default="", null=True)
     def __str__(self):
         return str(self.review_id) + ' - ' + str(self.category_product)
 
--- uploadapp/views.py
+++ uploadapp/views.py
@@ -29,11 +29,19 @@ def delete_category(request):
 def cleansing(csv_file):
     '''전처리 시작'''
     raw_data = pd.read_csv("." + csv_file, encoding='utf-8')
-    print(raw_data)
-    data = raw_data.filter(['Original Comments'])
-
+    # print(raw_data)
+    data = raw_data.filter(['Original Comments', 'Model Name', 'Model Code'])
+    # print("데이터 제거 전 1",data.shape,model_name.shape)
     '''중복 제거(1)'''
+    #중복이 확인되어 제거될 데이터의 인덱스번호 추출
+    # removed_index = list(set(data.index) - set(data.drop_duplicates(['Original Comments']).index))
+    # removed_index.sort(reverse=True)
+    #data(댓글)에서 제거된 인덱스에 맞추어 model_name(모델명) 데이터 제거
+    # for index_to_drop in removed_index:
+    #      model_name = model_name.drop(index_to_drop)
+    
     data = data.drop_duplicates(['Original Comments'])
+    # print("데이터 제거 후 1",data.shape,model_name.shape)
 
     '''불필요한 문자열 제거'''
     # html태그 제거
@@ -109,12 +117,40 @@ def cleansing(csv_file):
     data['Original Comments'] = data['Original Comments'].str.replace(pat=r'결제\s번호\s', repl=r'', regex=True)
     data['Original Comments'] = data['Original Comments'].str.replace(pat=r'구매\s번호\s', repl=r'', regex=True)
 
+
+
+
     '''중복 제거(2)'''
+    # data['index'] = data.index
+
+    # 데이터 전처리
     data['temp'] = data['Original Comments']
     data['temp'] = data['temp'].str.replace(' ', '')
-    data = data.drop_duplicates(['temp'], ignore_index=True)
+
+    # 중복 제거
+    data = data.drop_duplicates(['temp'])
     data = data.drop(['temp'], axis=1)
-    print(data)
+    
+
+    # 중복 제거 이전과 이후의 인덱스 비교하여 삭제된 행의 인덱스 리스트 구하기
+    # removed_index2 = list(set(data['index'].index) - set(unique_data['index']))
+    # removed_index2.sort(reverse=True)
+    # unique_data = unique_data.drop(['index'], axis=1)
+    # data['temp'] = data['Original Comments']
+    # data['temp'] = data['temp'].str.replace(' ', '')
+
+    # data = data.drop_duplicates(['temp'], ignore_index=True)
+    # unique_data = data.drop(['temp'], axis=1)
+
+    
+    # removed_index2 = list(set(data['Original Comments'].index) - set(data['temp'].index))
+    #data(댓글)에서 제거된 인덱스에 맞추어 model_name(모델명) 데이터 제거
+    # for index_to_drop2 in removed_index2:
+    #      model_name = model_name.drop(index_to_drop2)
+
+    # data = unique_data
+    # print("데이터 제거 후 2",data.shape,model_name.shape)
+    # print(data)
     return data
 
 
@@ -175,7 +211,8 @@ def upload_main(request):
                     upload_file_url = fs.url(filename)
                     dbframe = cleansing(upload_file_url)
                     fs.delete(str(BASE_DIR) + upload_file_url)
-
+                    
+            
                     # 중복 제거하기
                     dbreviews = Review.objects.filter(
                         category_product=request.POST.get('category_product')).values_list('review_content', flat=True)
@@ -183,7 +220,7 @@ def upload_main(request):
 
                     dbframe = pd.merge(dbreviews, dbframe, how='outer', indicator=True).query(
                         '_merge == "right_only"').drop(columns=['_merge'])
-                    print(dbframe)
+
                     # 현재 model의 category_product별로 최대값을 기준으로 review_number을 갱신하기 위한 변수 category_max_num
                     category_max_num = Review.objects.filter(
                         category_product=request.POST.get('category_product')).aggregate(temp=Max('review_number')).get(
@@ -192,11 +229,16 @@ def upload_main(request):
                         category_max_num = 0
                     dbframe.reset_index(inplace=True)
                     dbframe['index'] = dbframe['index'] + int(category_max_num) + 1
-
+                    # dbframe = pd.merge(dbframe, model_name, left_index=True, right_index=True, how='left')
+                    # print(dbframe)
+                    # print("model",model_name)
                     # 저장 부분
+                    print(dbframe)
                     review_obj = [Review(review_content=row['Original Comments'],
                     category_product=request.POST.get('category_product'),
-                    review_number=row['index']) for _, row in dbframe.iterrows()]
+                    review_number=row['index'],
+                    model_name=row['Model Name'],
+                    model_code=row['Model Code']) for _, row in dbframe.iterrows()]
                     Review.objects.bulk_create(review_obj)
                     request.session['message'] = '업로드가 완료되었습니다.'
                     url = reverse('uploadapp:upload') + f'?category_product={request.POST.get("category_product")}'
