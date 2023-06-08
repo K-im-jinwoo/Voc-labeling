56ce3a27c2c98e142948e3564d7b1aec4410a751
dongwoochoi <dong99woo@gmail.com>
2023-06-08 15:38:45 +0900

    리뷰 데이터 model_name.model_code별로 필터링 기능 추가

--- mainapp/models.py
+++ mainapp/models.py
@@ -33,9 +33,9 @@ class Review(models.Model):
     second_assign_user = models.TextField(default="0", null=True)
     model_name = models.TextField(default="", null=True)
     model_code = models.TextField(default="", null=True)
+
     def __str__(self):
         return str(self.review_id) + ' - ' + str(self.category_product)
-
 
 class FirstLabeledData(models.Model):
     first_labeled_id = models.AutoField(primary_key=True)
@@ -44,6 +44,8 @@ class FirstLabeledData(models.Model):
     first_labeled_expression = models.CharField(max_length=256)
     category_id = models.ForeignKey("Category", on_delete=models.CASCADE)
     review_id = models.ForeignKey("Review", on_delete=models.CASCADE)
+    model_name = models.TextField(default="", null=True)
+    model_code = models.TextField(default="", null=True)
 
     def __str__(self):
         return str(self.first_labeled_id) + ' - ' + str(self.first_labeled_emotion)
