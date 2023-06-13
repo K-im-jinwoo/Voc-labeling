fd4fa00876b3794deac80d66986792f2ba988176
zzuzzu0205 <zkakcnd@naver.com>
2023-06-13 19:30:32 +0900

    [add] status 오류 수정 중

--- LG_Project/settings/prod.py
+++ LG_Project/settings/prod.py
@@ -2,21 +2,23 @@ from .base import *
 
 import environ
 
-ALLOWED_HOSTS = ['43.200.38.56']
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+ALLOWED_HOSTS = ["43.200.38.56"]
+STATIC_ROOT = BASE_DIR / "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 DEBUG = False
 
 env = environ.Env()
-environ.Env.read_env(BASE_DIR / '.env')
+environ.Env.read_env(BASE_DIR / ".env")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": "5432",
     }
 }
