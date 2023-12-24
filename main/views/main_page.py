c028ce568173ca3ceca1d60f67b9fe475500d2f4
dywudmin <mindu6424@naver.com>
2023-12-24 19:10:02 +0900

    Modify - 메인페이지 작업 완료

--- main/views/main_page.py
+++ main/views/main_page.py
@@ -1,125 +1,92 @@
 from django.contrib.auth.models import User
-from django.db.models import Count, Q
+from django.db.models import Count, Q, Sum
 from django.shortcuts import render
-
 from main import models as main_models
 
 def main_page(request):
-    context = dict()
-    # 제품별 모델개수
-    product_names = main_models.Review.objects.values("category_product").distinct()
+    context = {}
+
+    # 제품별 모델 개수 가져오기
+    product_names = main_models.Review.objects.values("product__name").distinct()
     product_model_counts = []
     for product in product_names:
         model_name_count = (
             main_models.Review.objects.exclude(Q(model_name__isnull=True) | Q(model_name=""))
-            .filter(category_product=product["category_product"])
+            .filter(product__name=product["product__name"])
             .values("model_name")
             .distinct()
             .count()
         )
-        product_model_counts.append(
-            {
-                "category_product": product["category_product"],
-                "model_name_count": model_name_count,
-            }
-        )
+        product_model_counts.append({
+            "category_product": product["product__name"],
+            "model_name_count": model_name_count,
+        })
 
-    sorted_products = sorted(product_model_counts, key=lambda x: x["category_product"])
-    context["product_model_counts"] = sorted_products[:5]
+    context["product_model_counts"] = sorted(product_model_counts, key=lambda x: x["category_product"])[:5]
 
-    # 제품별 리뷰 총 개수
-    category_review_counts = main_models.Review.objects.values("category_product").annotate(
-        review_count=Count("review_content")
+    # 제품별 리뷰 총 개수 및 레이블링된 개수 계산
+    category_review_counts = main_models.Review.objects.values("product__name").annotate(
+        review_count=Count("content")
     )
     category_review_counts_labeled = (
-        main_models.Review.objects.filter(labeled_user_id__isnull=False)
-        .values("category_product")
-        .annotate(labeled_count=Count("review_content"))
+        main_models.Review.objects.filter(worked_user__isnull=False)
+        .values("product__name")
+        .annotate(labeled_count=Count("content"))
     )
 
     result_list = []
-
     for item in category_review_counts:
         matched_item = next(
-            (
-                x
-                for x in category_review_counts_labeled
-                if x["category_product"] == item["category_product"]
-            ),
+            (x for x in category_review_counts_labeled if x["product__name"] == item["product__name"]),
             None,
         )
         labeled_count = matched_item["labeled_count"] if matched_item else 0
-        percentage_labeled = (
-            round((labeled_count / item["review_count"]) * 100)
-            if item["review_count"] > 0
-            else 0
-        )
+        percentage_labeled = round((labeled_count / item["review_count"]) * 100) if item["review_count"] > 0 else 0
 
-        result_list.append(
-            {
-                "category_product": item["category_product"],
-                "total_reviews": item["review_count"],
-                "labeled_count": labeled_count,
-                "percentage_labeled": percentage_labeled,
-            }
-        )
+        result_list.append({
+            "category_product": item["product__name"],
+            "total_reviews": item["review_count"],
+            "labeled_count": labeled_count,
+            "percentage_labeled": percentage_labeled,
+        })
 
-    sorted_result_list = sorted(result_list, key=lambda x: x["category_product"])
-    context["result"] = sorted_result_list[:5]
+    context["result"] = sorted(result_list, key=lambda x: x["category_product"])[:5]
 
-    # 메인 첫번째
-    temp_user = User.objects.all()
-    # temp_user = User.objects.filter(is_superuser=False)
-    result_name = []
-    result_count = []
-    product_count = (
-        main_models.Category.objects.values("category_product").distinct().count()
-    )  # 제품갯수
-    user_count = temp_user.count()  # 유저수
-    review_count = main_models.Review.objects.count()  # 총리뷰수
+    # 기본 정보: 제품 수, 유저 수, 리뷰 수
+    product_count = main_models.Category.objects.values("product__name").distinct().count()
+    user_count = User.objects.count()
+    review_count = main_models.Review.objects.count()
 
     context["product_count"] = product_count
     context["user_count"] = user_count
     context["review_count"] = review_count
 
-    users_with_review_counts = User.objects.annotate(
-        review_count=Count("review")
-    )  # 아이디당 리뷰 몇개달았는지 수
-
-    total_review_count_by_users = 0  # 아이디당 리뷰 수 총 합
-    for user in users_with_review_counts:
-        total_review_count_by_users += user.review_count
-        
-    # user_ratio = int((total_review_count_by_users / review_count) * 100)
-    # context["review_ratio"] = round(
-    #     (total_review_count_by_users / review_count) * 100, 1
-    # )  # 아이디당 리뷰 수 총합 / 총리뷰수
-    if review_count != 0:
-        context["review_ratio"] = round(
-            (total_review_count_by_users / review_count) * 100, 1
-        )
+    # 리뷰 비율 계산
+    total_review_count = main_models.Review.objects.all().count()
+    labeled_reviews_count = main_models.Review.objects.filter(is_labeled=True).count()
+    if total_review_count != 0 and labeled_reviews_count != 0:
+        ratio = round(labeled_reviews_count / total_review_count * 100, 1)
+        context["review_ratio"] = ratio
     else:
-        # review_count가 0인 경우에 대한 처리
         context["review_ratio"] = 0
 
-    # 메인 세번째
-    user_name = []
-    user_result_count = []
-    user_ratios = []
+    # 유저별 리뷰 개수 및 비율
+    result_list = []
+    user_name_obj_list = User.objects.all().values_list()
+    user_name_list = user_name_obj_list.values("username")
+    total_review_count = main_models.Review.objects.all().count()
+    i = 0
+    for user_name in user_name_obj_list:
+        user_labeled_count = main_models.Review.objects.filter(worked_user=user_name).count()
+        user_ratio_percentage = 0
+        if total_review_count != 0:
+            user_ratio_percentage = int((user_labeled_count / total_review_count) * 100)
+        result_list.append((user_name_list[i]["username"], user_labeled_count, user_ratio_percentage))
+        i = i + 1
 
-    for user in users_with_review_counts:
-        user_name.append(user.username)
-        user_result_count.append(user.review_count)
-        # user_ratio_percentage = int((user.review_count / review_count) * 100)
-        if review_count != 0:
-            user_ratio_percentage = int((user.review_count / review_count) * 100)
-        else:
-            # review_count가 0인 경우에 대한 처리
-            user_ratio_percentage = 0
-        user_ratios.append(user_ratio_percentage)
-        result = zip(user_name, user_result_count, user_ratios)
-
-    result = sorted(result, key=lambda x: x[1], reverse=True)
-    context["result_list"] = result
+    # 결과 리스트를 리뷰 개수 기준으로 내림차순으로 정렬
+    result_list = sorted(result_list, key=lambda x: x[1], reverse=True)
+    context["result_list"] = result_list
+        
 
     return render(request, "main/main_page.html", context)
