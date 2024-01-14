9106fbadb746909070721e327c6fc46f6a325738
dywudmin <mindu6424@naver.com>
2024-01-15 00:17:34 +0900

    Modify - output 페이지 csv파일 다운로드 코드 생성

--- output/views.py
+++ output/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from io import BytesIO
+from io import BytesIO, StringIO
 import pandas as pd
 from django.http import HttpResponse
 import csv
@@ -12,8 +12,7 @@ from main import models as main_models
 def format_change(writer, category):
     writer.sheets[category.name].set_column("A:B", 15)  # Product_Group, Type 컬럼 넓이 변경
     writer.sheets[category.name].set_column("C:C", 10)  # Category 컬럼 넓이 변경
-    writer.sheets[category.name].set_column("D:F", 35)  # 키워드 컬럼 넓이 변경
-
+    writer.sheets[category.name].set_column("D:F", 35)  # 키워드 컬럼 넓이 변a
 # 제품 선택에 대한 데이터
 def data_by_select_product(select_product):
     # review_by_product(제품에 해당하는 리뷰 전체를 가져옴)
@@ -135,51 +134,75 @@ def output(request):
                     )
                     return response
 
-            # elif request.POST["export"] == ".csv export":
-            #     ####---- HttpResponse 설정 ----####
-            #     product = request.POST["product"]
-            #     response = HttpResponse(content_type="text/csv")
-            #     response["Content-Disposition"] = (
-            #         "attachment; filename="
-            #         + product
-            #         + "_"
-            #         + datetime.now().strftime("%Y-%m-%d_%I-%M-%S_%p")
-            #         + ".csv"
-            #     )
-            #     response.write("\ufeff".encode("utf8"))
+            elif request.POST["export"] == ".csv export":
+                # 선택한 제품의 작업된 데이터를 가져옴
+                labeled_data_by_product = main_models.LabelingData.objects.filter(
+                    category__product__name=select_product
+                )
 
-            #     ####---- csv 파일 만들기 ----####
-            #     writer = csv.writer(response)
-            #     reviews = list(
-            #         main_models.Review.objects.filter(
-            #             first_status=True, category_product=product
-            #         ).values_list("review_id", flat=True)
-            #     )
-            #     review_contents = list(
-            #         main_models.Review.objects.filter(
-            #             first_status=True, category_product=product
-            #         ).values_list("review_content", flat=True)
-            #     )
-            #     result = [[""]] * len(reviews)
-            #     for i in range(len(reviews)):
-            #         categorys = main_models.LabelingData.objects.filter(
-            #             review_id=reviews[i], category_id__category_product=product
-            #         ).values_list("category_id__category_middle", flat=True)
-            #         review_category = ""
-            #         for category in categorys:
-            #             review_category += category + "and"
-            #         review_category = review_category[:-3]
-            #         result[i] = [reviews[i], review_contents[i], review_category]
-            #     print(result[0])
+                # 선택한 제품에 해당하는 카테고리를 가져옴
+                categories_by_product = main_models.Category.objects.filter(product__name=select_product)
 
-            #     # 6. csv 파일 만들기
-            #     writer.writerow(["리뷰 번호", "리뷰 원문", "카테고리"])
-            #     for rlt in result:
-            #         writer.writerow(rlt)
-            #     return response
+                # emotion 데이터
+                emotions = main_models.Emotion.objects.all()
 
-            # else:
-            #     print("error")
+                with BytesIO() as b:
+                    writer = pd.ExcelWriter(b, engine="xlsxwriter")
+
+                    for category in categories_by_product: # [제품군에 해당하는 카테고리]
+                        keywords = {}
+                        counts = {}
+                        export_dict = {}
+                        for emotion in emotions: # [긍정, 부정, 중립 객체]
+                            kewords_in_labeled_data = labeled_data_by_product.filter(
+                                category=category, emotion=emotion
+                                ).values_list(
+                                "target", "phenomenon"
+                                ).distinct().order_by("target", "phenomenon")                            
+
+                            #### 같은 대상(target)을 딕셔너리 키로 묶기 ####
+                            keyword_dict = dict()
+                            for keword_in_labeled_data in kewords_in_labeled_data:
+                                target = keword_in_labeled_data[0] or ""
+                                phenomenon = keword_in_labeled_data[1] or ""
+                                if target not in keyword_dict:
+                                    keyword_dict[target] = list()
+                                if not any(phenomenon.startswith(keyword) for keyword in keyword_dict.get(target, [])):
+                                    keyword_dict[target].append(phenomenon)
+                                    
+                            export_keyword_by_category=list()
+                            for key, val_list in keyword_dict.items():
+                                for val in val_list:
+                                    export_keyword_by_category.append(key + " AND " + val)
+
+                            # count = 감정별 키워드쌍의 개수를 세고 제일 많은 것을 골라 행을 채우는 용도
+                            counts[emotion.e_name + "_keyword"] = len(export_keyword_by_category)
+
+                            # 감정별 데이터 쌍을 저장
+                            keywords[emotion.e_name + "_keyword"] = export_keyword_by_category
+
+                        max_count = max(counts.values())
+                        export_dict = {
+                            "Product_Group": [select_product] * max_count,
+                            "Type": ["3F_Ergonomics"] * max_count,
+                            "Category": [category.name] * max_count,
+                            "긍정 키워드": keywords["positive_keyword"],
+                            "부정 키워드": keywords["negative_keyword"],
+                            "중립 키워드": keywords["neutral_keyword"],
+                        }
+                        df = pd.DataFrame.from_dict(export_dict, orient="index")
+                        df = df.transpose()
+                        df.to_excel(writer, sheet_name=category.name, index=False)
+                        format_change(writer, category)
+                    writer.save()
+                    encoded_filename = urllib.parse.quote(select_product)
+                    content_type = "application/vnd.ms-excel"
+                    response = HttpResponse(b.getvalue(), content_type=content_type)
+                    response["Content-Disposition"] = (
+                        'attachment; filename="' + encoded_filename + '.csv"'
+                    )
+                    return response
+
         else:
             print("error")
 
